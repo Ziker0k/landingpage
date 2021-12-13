68b948f9dc466681e0e086468f1794e28b7df676
Ziker0k <94623900+Ziker0k@users.noreply.github.com>
2021-12-14 01:22:47 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,29 +1,18 @@
+function validate() {
+        let a = document.forms["myForm"]["name"].value;
+        if (a == "") {
+            alert("Fill the name...");
+            return false;
 
-const form = document.querySelector("form"),
-    statusTxt = form.querySelector(".button-area span");
-form.onsubmit = (e)=>{
-    e.preventDefault();
-    statusTxt.style.color = "#0D6EFD";
-    statusTxt.style.visibility = "visible";
-    statusTxt.innerText = "Sending your message...";
-    form.classList.add("disabled");
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "php/message.php", true);
-    xhr.onload = ()=>{
-        if(xhr.readyState == 4 && xhr.status == 200){
-            let response = xhr.response;
-            if(response.indexOf("required") != -1 || response.indexOf("valid") != -1 || response.indexOf("failed") != -1){
-                statusTxt.style.color = "red";
-            }else{
-                form.reset();
-                setTimeout(()=>{
-                    statusTxt.style.visibility = "hidden";
-                }, 3000);
-            }
-            statusTxt.innerText = response;
-            form.classList.remove("disabled");
+        }
+        let с = document.forms["myForm"]["email"].value;
+        if (с == "") {
+            alert("Fill the email...");
+            return false;
+        }
+        let x = document.forms["myForm"]["message"].value;
+        if (x == "") {
+            alert("Write message...");
+            return false;
         }
     }
-    let formData = new FormData(form);
-    xhr.send(formData);
-}
